28bf6cc09d600862e7567ad5d22e3dea183193b1
szymqn <koodziejskisz@outlook.com>
2023-01-02 14:57:55 +0100

    add user register

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from user.views import CustomLogin
+from user.views import CustomLogin, RegisterPage
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', CustomLogin.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', RegisterPage.as_view(), name='register'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth import login
 from django.urls import reverse_lazy
 
+from django.views.generic.edit import FormView
+
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import UserCreationForm
 
 
 class CustomLogin(LoginView):
@@ -11,3 +15,21 @@ class CustomLogin(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('tasks')
+
+
+class RegisterPage(FormView):
+    template_name = 'user/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage, self).get(*args, **kwargs)
